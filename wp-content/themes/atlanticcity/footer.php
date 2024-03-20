304a2fa53a2ce2486a1eaa03f19f4e69d0c433fa
cquispepattern <cristhian@pattern.pe>
2024-03-20 01:22:59 -0500

    Height

--- wp-content/themes/atlanticcity/footer.php
+++ wp-content/themes/atlanticcity/footer.php
@@ -381,7 +381,6 @@ observer.observe(document.body, { childList: true, subtree: true });
             spaceBetween: 0,
             watchSlidesVisibility: true,
             watchSlidesProgress: true,
-            autoHeight: true,
             loop: false,
             navigation: {
                 prevEl: ".swiper-gallery-button-prev",
